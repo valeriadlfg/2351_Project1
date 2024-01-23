f0941120a5c1b49114d98192a31ebe1263e6954f
valeriadlfg <valeria.dlf31@gmail.com>
2024-01-23 16:36:17 -0600

    added variables- worked on 2.4 assigment

--- app.js
+++ app.js
@@ -3,3 +3,72 @@ console.log("What has been your biggest challenge in this career?") ;
 document.write(5+5);
 
 document.getElementById("demo").innerHTML = 5 + 6;
+
+    //Step one, creating variable values to the console 
+let myname = "Val";
+let answer = "Thats me!";
+
+console.log("My Name:", myname);
+console.log("Answer:", answer);
+
+
+    //Step two, create a global variable
+var funfact = "A crocodile cannot stick its tongue out.";
+console.log("Fun Fact:", funfact);
+
+    //Step three, create a block variable
+        
+        //Using const as a variable
+    
+    {
+        const carModel = "Tesla Model S";
+        const carYear = 2024;
+    
+        console.log("Car Model:", carModel);
+        console.log("Car Year:", carYear);
+    }
+
+            //Using let as a variable
+
+            {
+                let carModel = "Toyota Camry";
+                let carYear = 2023;
+            
+                
+                console.log("Car Model:", carModel);
+                console.log("Car Year:", carYear);
+            }
+
+            //Step four, performing mathematical operations
+let x = 150;
+let y = 75;
+            
+let sum = x + y;
+            
+console.log("Sum of x and y:", sum);
+
+            //Step five, creating a string variable
+
+let StringVariable = "Hello!";
+
+console.log("String Variable:", StringVariable);
+
+            //Step six, create an object variable, assign values
+
+let myself = {
+                firstName: "Valerie",
+                lastName: "De la Fuente",
+                age: 23,
+                student: true
+            };
+            
+            
+            console.log("Person Object variable:", myself);
+
+const MexicoCityInfo = {
+                cityName: "Mexico City",
+                country: "Mexico",
+                    
+            };
+            
+            console.log("Mexico City Information Object:", MexicoCityInfo);
